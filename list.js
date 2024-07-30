da8fe8b3b7208ef52f3693a41202cc160dd06877
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2024-07-30 11:15:24 +0700

    Update list.js

--- list.js
+++ list.js
@@ -1,1 +1,1 @@
-const subscribers = ["Mystery Box 1","Mystery Box 2","Mystery Box 3","Mystery Box 4","Mystery Box 5"];
+const subscribers = ["Mystery Box 1","Mystery Box 2","Mystery Box 3","Mystery Box 4","Mystery Box 5","Mystery Box 6","Mystery Box 7","Mystery Box 8","Mystery Box 9","Mystery Box 0"];
